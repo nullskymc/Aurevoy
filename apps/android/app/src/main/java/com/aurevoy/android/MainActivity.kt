58299096dee4c1dbb92ed7303da8f76515b3ca0b
nullskymc <nullskymc@hotmail.com>
2026-06-21 15:42:25 +0800

    fix(android): add missing import for Context and Intent in MainActivity

--- apps/android/app/src/main/java/com/aurevoy/android/MainActivity.kt
+++ apps/android/app/src/main/java/com/aurevoy/android/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.aurevoy.android
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
